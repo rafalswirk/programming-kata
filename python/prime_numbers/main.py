99dfb5ee9fee47a3d17642aa353fe8cf4191dae7
Rafal <rafalswirk@outlook.com>
2021-04-12 08:21:44 +0200

    - Implementing first kata - prime numbers

---
+++ python/prime_numbers/main.py
@@ -0,0 +1,15 @@
+
+def calculatePrimeNumbers(number_of_tests):
+    if number_of_tests < 1 or number_of_tests > 10000 :
+        print('Invalid number of tests. Please enter value between 1..10000')
+        return
+    bruteForcePrimeNumbers(number_of_tests)
+
+def bruteForcePrimeNumbers(number_to_check) :
+    for i in range(number_to_check) :
+        if i % number_to_check = 0
+            return false
+    return true
+
+
+calculatePrimeNumbers(-1)
