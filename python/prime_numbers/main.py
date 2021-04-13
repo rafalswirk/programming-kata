3d099951f2e35259ae3133bde8c072323b431fab
Rafał Świrk <rafalswirk@outlook.com>
2021-04-13 11:50:02 +0200

    - Implemented prime numbers check

--- python/prime_numbers/main.py
+++ python/prime_numbers/main.py
@@ -1,15 +1,14 @@
-
-def calculatePrimeNumbers(number_of_tests):
-    if number_of_tests < 1 or number_of_tests > 10000 :
-        print('Invalid number of tests. Please enter value between 1..10000')
-        return
-    bruteForcePrimeNumbers(number_of_tests)
+def isPrimeNumber(number_to_check):
+    return bruteForcePrimeNumbers(number_to_check)
 
 def bruteForcePrimeNumbers(number_to_check) :
-    for i in range(number_to_check) :
-        if i % number_to_check = 0
-            return false
-    return true
+    if number_to_check < 2 :
+        return False
+    
+    for i in range(2, number_to_check) :
+        if number_to_check % i == 0 :
+            return False
+    return True
 
 
-calculatePrimeNumbers(-1)
+print(isPrimeNumber(20002))
