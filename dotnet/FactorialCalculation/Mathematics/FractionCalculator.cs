8a7da89c7edf179a1f5536df39a6e3bb876350ae
Rafał Świrk <rafalswirk@outlook.com>
2021-04-30 14:39:17 +0200

    - Implementing factorial calulation

--- dotnet/FactorialCalculation/Mathematics/FractionCalculator.cs
+++ dotnet/FactorialCalculation/Mathematics/FractionCalculator.cs
@@ -6,7 +6,13 @@ namespace Mathematics
     {
         public int Calculate(int number)
         {
-            throw new NotImplementedException();
+            if(number < 1)
+                throw new ArgumentException("Input argument should be >= 1");
+
+            var result = 1;
+            for(int i = 1; i <= number; i++)
+                 result *= i;
+            return result;
         }
     }
 }
