e3f692465c18389d4f0a622c0464284492a5a98e
Rafał Świrk <rafalswirk@outlook.com>
2021-09-14 14:44:50 +0200

    - Added empty test for checking last digit of pow number

---
+++ dotnet/LastDigit/UnitTests/TestLastDigitOfPowNumber.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestLastDigitOfPowNumber
+    {
+        [TestMethod]
+        public void GetLastDigitFromPowNumber()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
