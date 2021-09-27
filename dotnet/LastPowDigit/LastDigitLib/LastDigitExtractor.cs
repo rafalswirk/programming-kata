dc22e9747549f50cb68689dd49f672bb8882f932
Rafal <rafalswirk@outlook.com>
2021-09-27 06:32:23 +0200

    - Implementing main algorithm

---
+++ dotnet/LastPowDigit/LastDigitLib/LastDigitExtractor.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace LastDigitLib
+{
+    public class LastDigitLib
+    {
+        public string ProcessData(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
