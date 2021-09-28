cef526a01c3d5aa98b3a05ca659c1a86371572ed
Rafal <rafalswirk@outlook.com>
2021-09-28 06:02:59 +0200

    - Implementing main algorithm for processing input

--- dotnet/LastPowDigit/LastDigitLib/LastDigitExtractor.cs
+++ dotnet/LastPowDigit/LastDigitLib/LastDigitExtractor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 namespace LastDigitLib
 {
@@ -9,8 +10,28 @@ namespace LastDigitLib
             var splittedValues = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
             int numberOfTests;
             if(!int.TryParse(splittedValues[0], out numberOfTests))
-                throw new ArgumentException();
-            throw new NotImplementedException();
+                throw new ArgumentException("Invalid number of tests");
+            var results = new StringBuilder();
+            for (int i = 1; i <= numberOfTests; i++)
+            {
+                var rawCaseData = splittedValues[i].Trim().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+                if(rawCaseData.Length != 2)
+                    throw new ArgumentException("Invalid input data for calculating power");
+
+                double numberToBeRaisedToPower;
+                if(!double.TryParse(rawCaseData[0], out numberToBeRaisedToPower))
+                    throw new ArgumentException($"Cannot read number to be raised to power! Raw data:{rawCaseData[0]}");
+                
+                double specificPower;
+                if(!double.TryParse(rawCaseData[1], out specificPower))
+                    throw new ArgumentException($"Cannot read number to be raised to power! Raw data:{rawCaseData[1]}");
+
+                var powResult = (int)Math.Pow(numberToBeRaisedToPower, specificPower);
+                results.AppendLine(powResult.ToString());
+            }
+
+            return results.ToString();
         }
     }
 }
