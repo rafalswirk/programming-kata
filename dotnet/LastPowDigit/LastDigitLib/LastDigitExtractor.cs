349d727d98f87fcae311f42a1e4d2b3b869dd616
Rafal <rafalswirk@outlook.com>
2021-09-28 05:45:16 +0200

    - Getting number of tests from string input

--- dotnet/LastPowDigit/LastDigitLib/LastDigitExtractor.cs
+++ dotnet/LastPowDigit/LastDigitLib/LastDigitExtractor.cs
@@ -6,6 +6,10 @@ namespace LastDigitLib
     {
         public string ProcessData(string input)
         {
+            var splittedValues = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            int numberOfTests;
+            if(!int.TryParse(splittedValues[0], out numberOfTests))
+                throw new ArgumentException();
             throw new NotImplementedException();
         }
     }
