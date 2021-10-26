d27093a2d071eb68be5a1db26a1dbea878375dd5
Rafi <rafalswirk@outlook.com>
2021-10-26 05:41:39 +0200

    - Adding class for compressing algorithm

---
+++ dotnet/StringCompression/StringCompressionLib/StringCompression.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace StringCompressionLib
+{
+    public class StringCompression
+    {
+        public string Compress(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
