9c7e87ffb0439bb0fefe252c25d3fd555b061653
Rafi <rafalswirk@outlook.com>
2021-11-06 12:20:38 +0100

    - Adding console project for demonstration of compressing algorithm

---
+++ dotnet/StringCompression/StringCompressionApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringCompressionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
