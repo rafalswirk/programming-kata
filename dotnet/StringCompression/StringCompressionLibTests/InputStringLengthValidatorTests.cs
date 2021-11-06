70358540ed86d916cae0cc8589a1454eedeb2012
Rafi <rafalswirk@outlook.com>
2021-11-06 12:40:00 +0100

    - Generating files for handling algorithm input

---
+++ dotnet/StringCompression/StringCompressionLibTests/InputStringLengthValidatorTests.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace StringCompressionLibTests
+{
+    public class InputStringLengthValidatorTests
+    {
+        [Fact]
+        public void CheckToLongInput()
+        {
+            throw new NotImplementedException();
+        }        
+
+        [Fact]
+        public void CheckToShortInput()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
