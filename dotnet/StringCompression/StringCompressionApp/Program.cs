ccb917bbdc121e909358c89930d3a29fa9a32d23
Rafi <rafalswirk@outlook.com>
2021-11-17 07:57:51 +0100

    - Implementing console compression app

--- dotnet/StringCompression/StringCompressionApp/Program.cs
+++ dotnet/StringCompression/StringCompressionApp/Program.cs
@@ -6,7 +6,17 @@ namespace StringCompressionApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numberOfTests = Console.ReadLine();
+            if(int.TryParse(numberOfTests, out int testsCount ))
+            {
+                throw new NotImplementedException();
+                //Add reference to compresison lib!!!!
+                // var compression = new StringCompression
+                // for (int i = 0; i < testsCount; i++)
+                // {
+                    
+                // }
+            }
         }
     }
 }
