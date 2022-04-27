d89c3ceadbc43e8a713058594a172ac8d7e51a76
rafalswirk <rafalswirk@outlook.com>
2022-04-27 07:49:07 +0200

    - Implementing demo app

--- dotnet/WPF/Misc/ICommandImplementation/ICommandDemo/MainWindow.xaml.cs
+++ dotnet/WPF/Misc/ICommandImplementation/ICommandDemo/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ICommandDemo.MVVM;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +20,31 @@ namespace ICommandDemo
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private int accountLimit = 200;
+        public int AccountLimit 
+        { 
+            get => accountLimit; 
+            set 
+            {
+                accountLimit = value; 
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AccountLimit))); 
+            } 
+        }
+        public int CashToWithdraw { get; set; } = 60;
+        public ICommand WithdrawCommand { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
         public MainWindow()
         {
+            WithdrawCommand = new RelayCommand(_ =>
+            {
+                AccountLimit -= CashToWithdraw;
+            }, _ => CashToWithdraw <= AccountLimit);
             InitializeComponent();
         }
+
     }
 }
