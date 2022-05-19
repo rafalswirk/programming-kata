a7cc3a53bf7a3b3db4a771ba8bd2e0db76fe0b33
rafalswirk <rafalswirk@outlook.com>
2022-05-19 08:35:15 +0200

    - Input bindings for the rest of animals

--- dotnet/WPF/Misc/InputBindingsDemo/DemoApp/MainWindow.xaml.cs
+++ dotnet/WPF/Misc/InputBindingsDemo/DemoApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DemoApp
 {
@@ -23,16 +23,52 @@ namespace DemoApp
     {
         MediaPlayer mediaElement = new MediaPlayer();
 
-        public RelayCommand WolfSound { get; set; }
+        public RelayCommand WolfSound { get; private set; }
+        public RelayCommand BeeSound { get; private set; }
+        public RelayCommand CatSound { get; private set; }
+        public RelayCommand DonkeySound { get; private set; }
+        public RelayCommand FrogSound { get; private set; }
+        public RelayCommand LionSound { get; private set; }
+        public RelayCommand OwlSound { get; private set; }
+
         public MainWindow()
         {
             WolfSound = new RelayCommand(() =>
             {
-                mediaElement.Open(new Uri(@"AnimalSounds\wolf.mp3", UriKind.Relative));
-                mediaElement.Play();
+                PlaySound("wolf.mp3");
+            });
+            BeeSound = new RelayCommand(() =>
+            {
+                PlaySound("bee.mp3");
+            });
+            CatSound = new RelayCommand(() =>
+            {
+                PlaySound("cat.mp3");
+            });
+            DonkeySound = new RelayCommand(() =>
+            {
+                PlaySound("donkey.mp3");
+            });
+            FrogSound = new RelayCommand(() =>
+            {
+                PlaySound("frogs.mp3");
+            });
+            LionSound = new RelayCommand(() =>
+            {
+                PlaySound("lion.mp3");
+            });
+            OwlSound = new RelayCommand(() =>
+            {
+                PlaySound("owl.mp3");
             });
             
             InitializeComponent();
+        }
+
+        private void PlaySound(string soundName)
+        {
+            mediaElement.Open(new Uri(Path.Combine("AnimalSounds", soundName), UriKind.Relative));
+            mediaElement.Play();
         }
     }
 }
