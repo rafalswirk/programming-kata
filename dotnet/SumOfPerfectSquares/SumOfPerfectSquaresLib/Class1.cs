596c076bd1e07f6c762a837984a7333129e44013
rafalswirk <rafalswirk@outlook.com>
2022-07-25 06:50:33 +0200

    - Init commit

---
+++ dotnet/SumOfPerfectSquares/SumOfPerfectSquaresLib/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace SumOfPerfectSquaresLib;
+public class Class1
+{
+
+}
---
+++ dotnet/SumOfPerfectSquares/SumOfPerfectSquaresTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace SumOfPerfectSquaresTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
