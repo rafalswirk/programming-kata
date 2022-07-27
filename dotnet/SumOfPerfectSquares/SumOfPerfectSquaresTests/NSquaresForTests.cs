ad71046e89b0d34df8b65761de4bf5ec2516cf10
rafalswirk <rafalswirk@outlook.com>
2022-07-27 06:11:45 +0200

    - Improving unit test naming

--- dotnet/SumOfPerfectSquares/SumOfPerfectSquaresTests/NSquaresForTests.cs
+++ dotnet/SumOfPerfectSquares/SumOfPerfectSquaresTests/NSquaresForTests.cs
@@ -2,7 +2,7 @@ using SumOfPerfectSquaresLib;
 
 namespace SumOfPerfectSquaresTests;
 
-public class Tests
+public class NSquaresForTests
 {
     [SetUp]
     public void Setup()
@@ -13,7 +13,7 @@ public class Tests
     [TestCase(2, 17)]
     [TestCase(4, 15)]
     [TestCase(1, 16)]
-    public void Test1(int expected, int input)
+    public void NSquaresFor_InputNumber_ReturnsSmallestListOfPerfectSquares(int expected, int input)
     {
         Assert.AreEqual(expected, SumOfSquares.NSquaresFor(input));
     }
