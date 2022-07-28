4ea72e810faca244206312f355b973529257f656
rafalswirk <rafalswirk@outlook.com>
2022-07-28 05:45:03 +0200

    - More test cases - Updating assertion inside test

--- dotnet/SumOfPerfectSquares/SumOfPerfectSquaresTests/NSquaresForTests.cs
+++ dotnet/SumOfPerfectSquares/SumOfPerfectSquaresTests/NSquaresForTests.cs
@@ -10,11 +10,13 @@ public class NSquaresForTests
     }
 
     [Test]
-    [TestCase(2, 17)]
     [TestCase(4, 15)]
     [TestCase(1, 16)]
+    [TestCase(2, 17)]
+    [TestCase(2, 18)]
+    [TestCase(3, 19)]
     public void NSquaresFor_InputNumber_ReturnsSmallestListOfPerfectSquares(int expected, int input)
     {
-        Assert.AreEqual(expected, SumOfSquares.NSquaresFor(input));
+        Assert.That(SumOfSquares.NSquaresFor(input), Is.EqualTo(expected));
     }
 }
