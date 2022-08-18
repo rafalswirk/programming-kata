d796f49fbdb96331013213a084de4a5ab9356a6a
rafalswirk <rafalswirk@outlook.com>
2022-08-18 21:43:23 +0200

    - Added ContainsDuplicate problem

---
+++ dotnet/NeetCode/Problems.Test/ArraysAndHashingTests/ContainsDuplicatesTest.cs
@@ -0,0 +1,22 @@
+﻿using Problems.ArraysAndHashing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Test.ArraysAndHashingTests
+{
+    [TestFixture]
+    public class ContainsDuplicatesTest
+    {
+        [TestCase(new int[] { 1, 2, 3, 1 }, true)]
+        [TestCase(new int[] { 1, 2, 3, 4 }, false)]
+        [TestCase(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }, true)]
+        public void ContainsDuplicates(int[] input, bool result)
+        {
+            var containsDuplicate = new ContainsDuplicate();
+            Assert.That(containsDuplicate.Check(input), Is.EqualTo(result));
+        }
+    }
+}
---
+++ dotnet/NeetCode/Problems/ArraysAndHashing/ContainsDuplicate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.ArraysAndHashing
+{
+    public class ContainsDuplicate
+    {
+        public bool Check(int[] data)
+        {
+            return data.GroupBy(d => d).Any(g => g.Count() > 1);
+        }
+    }
+}
