e1569c8ec9f2d8c2ef800362d61b95768fcc2efd
rafalswirk <rafalswirk@outlook.com>
2022-08-19 13:36:12 +0200

    - Added empty class + test for next problem

---
+++ dotnet/NeetCode/Problems.Test/ArraysAndHashingTests/ValidAnagramTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Test.ArraysAndHashingTests
+{
+    [TestFixture]
+    public class ValidAnagramTest
+    {
+        [Test]
+        public void IsValidAnagramTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ dotnet/NeetCode/Problems/ArraysAndHashing/ValidAnagram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.ArraysAndHashing
+{
+    public class ValidAnagram
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
