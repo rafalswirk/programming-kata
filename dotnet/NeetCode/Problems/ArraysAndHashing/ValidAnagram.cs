9027a23f63319d233f15b271aaa8c61c0aa61a07
rafalswirk <rafalswirk@outlook.com>
2022-08-20 06:54:01 +0200

    - Implemented anagaram checking

--- dotnet/NeetCode/Problems/ArraysAndHashing/ValidAnagram.cs
+++ dotnet/NeetCode/Problems/ArraysAndHashing/ValidAnagram.cs
@@ -16,7 +16,29 @@ namespace Problems.ArraysAndHashing
             }
             var sMap = new Dictionary<char, int>();
             var tMap = new Dictionary<char, int>();
-            throw new NotImplementedException();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!sMap.ContainsKey(s[i]))
+                    sMap.Add(s[i], 0);
+                sMap[s[i]]++;
+
+                if (!tMap.ContainsKey(t[i]))
+                    tMap.Add(t[i], 0);
+                tMap[t[i]]++;
+            }
+
+            foreach (var key in sMap.Keys)
+            {
+                if (!tMap.TryGetValue(key, out int tValue))
+                    return false;
+                if (sMap[key] != tValue)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
