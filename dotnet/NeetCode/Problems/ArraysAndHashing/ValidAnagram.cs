fee498b89c1fc8e96fae9908eb96bcfd602476e4
rafalswirk <rafalswirk@outlook.com>
2022-08-20 05:56:37 +0200

    - Starting of anagram logic implementation

--- dotnet/NeetCode/Problems/ArraysAndHashing/ValidAnagram.cs
+++ dotnet/NeetCode/Problems/ArraysAndHashing/ValidAnagram.cs
@@ -10,6 +10,12 @@ namespace Problems.ArraysAndHashing
     {
         public bool IsAnagram(string s, string t)
         {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+            var sMap = new Dictionary<char, int>();
+            var tMap = new Dictionary<char, int>();
             throw new NotImplementedException();
         }
     }
