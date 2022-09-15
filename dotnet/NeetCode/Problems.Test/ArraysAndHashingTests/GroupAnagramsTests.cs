be216205b032ce26cf60333f6db6678a91cbc18e
rafalswirk <rafalswirk@outlook.com>
2022-09-15 06:14:51 +0200

    - Implemented group anagram problem

---
+++ dotnet/NeetCode/Problems.Test/ArraysAndHashingTests/GroupAnagramsTests.cs
@@ -0,0 +1,50 @@
+﻿using Problems.ArraysAndHashing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Test.ArraysAndHashingTests
+{
+    /// <summary>
+    /// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+    /// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+    /// </summary>
+    [TestFixture]
+    public class GroupAnagramsTests
+    {
+        [TestCaseSource(nameof(TestData))]
+        public void GroupAnagramsTest(string[] input, List<List<string>> result)
+        {
+            var alghoritm = new GroupAnagrams();
+
+            var grouppedAnagrams = alghoritm.Group(input);
+
+            Assert.That(grouppedAnagrams.Count, Is.EqualTo(result.Count));
+            foreach (var group in grouppedAnagrams)
+            {
+                var resultGroup = result.Single(r => r.Count == group.Count);
+                foreach (var item in resultGroup)
+                {
+                    Assert.That(group.Contains(item), Is.True);
+                }
+            }
+
+        }
+
+        private static object[] TestData =
+        {
+            new object[]
+            {
+                new string[] { "eat", "tea", "tan", "ate", "nat", "bat" },
+                new List<List<string>> 
+                { 
+                    new List<string> { "bat" }, 
+                    new List<string> { "nat", "tan" },
+                    new List<string> { "ate","eat","tea" },
+                }
+            }
+        };
+    }
+}
---
+++ dotnet/NeetCode/Problems/ArraysAndHashing/GroupAnagrams.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Problems.ArraysAndHashing
+{
+    public class GroupAnagrams
+    {
+        public List<List<string>> Group(string[] input)
+        {
+            var dictionary = new Dictionary<byte[], List<string>>(new ByteArrayComparer()); 
+            foreach (var inputItem in input)
+            {
+                var characters = new byte[26];
+                foreach (var character in inputItem)
+                {
+                    characters[character - 'a']++;
+                }
+                if(!dictionary.Keys.Any(k => k.SequenceEqual(characters)))
+                    dictionary.Add(characters, new List<string>());
+                dictionary[characters].Add(inputItem);
+            }
+            return dictionary.Values.ToList();
+        }
+    }
+
+    public class ByteArrayComparer : EqualityComparer<byte[]>
+    {
+        public override bool Equals(byte[]? x, byte[]? y)
+        {
+            if (x is null || y is null)
+                return x == y;
+            if (x.Length != y.Length)
+                return false;
+
+            return x.SequenceEqual(y);
+        }
+
+        public override int GetHashCode([DisallowNull] byte[] obj)
+        {
+            int result = 17;
+            for (int i = 0; i < obj.Length; i++)
+            {
+                unchecked
+                {
+                    result = result * 23 + obj[i];
+                }
+            }
+            return result;
+        }
+    }
+}
