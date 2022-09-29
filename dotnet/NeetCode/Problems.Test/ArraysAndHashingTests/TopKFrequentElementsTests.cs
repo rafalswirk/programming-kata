cd23fc545d6cad6d71f2f06c43abb4ab2225cb6f
rafalswirk <rafalswirk@outlook.com>
2022-09-29 06:41:56 +0200

    - Implemented get top k frequent elements problem (without linq usage)

---
+++ dotnet/NeetCode/Problems.Test/ArraysAndHashingTests/TopKFrequentElementsTests.cs
@@ -0,0 +1,31 @@
+﻿using Problems.ArraysAndHashing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Test.ArraysAndHashingTests
+{
+    /// <summary>
+    /// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+    /// It is guaranteed that the answer is unique.
+    /// </summary>
+    [TestFixture]
+    public class TopKFrequentElementsTests
+    {
+        [TestCase(new int[] { 1, 1, 1, 2, 2, 3 }, 2, new int[] {1, 2 })]
+        public void TopKFrequentTest(int[] input, int k, int[] expectedResult)
+        {
+            var topKFrequent = new TopKFrequentElements();
+
+            int[] elements = topKFrequent.GetElements(input, k);
+
+            Assert.That(elements.Count(), Is.EqualTo(k));
+            for (int i = 0; i < k; i++)
+            {
+                Assert.That(expectedResult.Contains(elements[i]), Is.True);
+            }
+        }
+    }
+}
---
+++ dotnet/NeetCode/Problems/ArraysAndHashing/TopKFrequentElements.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.ArraysAndHashing
+{
+    public class TopKFrequentElements
+    {
+        public int[] GetElements(int[] input, int k)
+        {
+            var elementsCounter = new Dictionary<int, int>();
+            foreach (var value in input)
+            {
+                if(!elementsCounter.ContainsKey(value))
+                    elementsCounter.Add(value, 1);
+                else
+                    elementsCounter[value]++;
+            }
+
+            var frequency = new List<List<int>>(input.Length + 1);
+            for (int i = 0; i < input.Length + 1; i++)
+            {
+                frequency.Add(new List<int>(input.Length));
+            }
+
+            foreach (var key in elementsCounter.Keys)
+                frequency[elementsCounter[key]].Add(key);
+
+            var result = new List<int>();
+            for (int i = frequency.Count -1; i  > 0; i--)
+            {
+                if (frequency[i].Count > 0)
+                    result.AddRange(frequency[i]);
+                if (result.Count >= k)
+                    return result.ToArray();
+            }
+
+            return Array.Empty<int>();
+        }
+    }
+}
